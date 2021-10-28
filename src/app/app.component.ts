ee3289234cde4177abeb6d2e303ae3029787be83
Muhammad Abdullah <mabdullah.se@gmail.com>
2021-10-28 23:25:06 +0500

    WYSIWYG HTML Editor | Froala Upload Multiple Images using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'froala-with-multiple-upload-images-base64';
+  public editorContent: string = 'Im is Ready!';
+
+  public froalaOptions: Object = {
+    fontFamily: {
+      "'Source Serif Pro', serif": 'Source Serif Pro',
+      'Roboto,sans-serif': 'Roboto',
+      'Oswald,sans-serif': 'Oswald',
+      'Montserrat,sans-serif': 'Montserrat',
+      "'Open Sans Condensed',sans-serif": 'Open Sans Condensed',
+    },
+    fontFamilyDefaultSelection: 'Source Serif Pro',
+    fontFamilySelection: true,
+
+    events: {
+      'image.beforeUpload': function (files: any) {
+        let editor: any = this;
+        if (files.length) {
+          let currentImage = editor.image.get();
+          var reader = new FileReader();
+          reader.onload = function (e: any) {
+            var result = e.target.result;
+            editor.image.insert(result, null, null, currentImage);
+          };
+          reader.readAsDataURL(files[0]);
+        }
+        editor.popups.hideAll();
+        return false;
+      },
+      'image.error': function (error: any) {},
+    },
+  };
+
+  myForm: FormGroup;
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      content: new FormControl('Im is Ready!'),
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log(form.value.content);
+    this.editorContent = form.value.content;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import "froala-editor/js/plugins.pkgd.min.js";
+import "froala-editor/js/plugins/image.min.js";
+import "froala-editor/js/plugins/word_paste.min.js";
+import "froala-editor/js/plugins/table.min.js";
+import "froala-editor/js/plugins/font_size.min.js";
+import "froala-editor/js/plugins/line_breaker.min.js";
+import "froala-editor/js/plugins/line_height.min.js";
+import "froala-editor/js/plugins/lists.min.js";
+import "froala-editor/js/plugins/paragraph_format.min.js";
+import "froala-editor/js/plugins/quote.min.js";
+import "froala-editor/js/plugins/special_characters.min.js";
+import "froala-editor/js/plugins/font_family.min.js";
+
+// Import Angular plugin.
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
